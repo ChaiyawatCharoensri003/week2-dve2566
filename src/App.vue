62155f79c3cdb3c87b1fd92bc954c0f0801cfb60
ChaiyawatCharoensri003 <nra79318@gmail.com>
2024-05-28 12:41:06 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+ref
+const name = "ONENA";
+const age = 999;
+</script>
+
+<style lang="scss" scoped></style>
